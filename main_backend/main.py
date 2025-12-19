974c2a33f64334abd539e5bc6af1591560d6bf51
unknown <qkdrk7777775@gmail.com>
2025-12-19 11:43:47 +0900

    feat: cors 임시 수정

--- main_backend/main.py
+++ main_backend/main.py
@@ -49,7 +49,9 @@ origins = [
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    # allow_origins=origins,
+    # allow_origins=["*"],
+    allow_origin_regex=".*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
